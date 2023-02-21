d8b9614d75ed7a6c0fb229c713402f37bb367952
Dimitar Janevski <dimitar.j27@gmail.com>
2023-02-21 15:47:09 -0700

    begin lobby page

--- frontend/src/pages/Lobby.tsx
+++ frontend/src/pages/Lobby.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Lobby() {
-  return <div>Lobby</div>;
+  const Card = () => {
+    return <div>card</div>;
+  };
+  return (
+    <div className="bg-blue w-[100vw] h-[100vh] py-20 px-10 lg:px-80">
+      <div className="font-display text-white text-6xl">Lobby</div>
+      <div className="font-sans text-white text-xl">Game ID: </div>
+      {Card()}
+    </div>
+  );
 }
 
 export default Lobby;
