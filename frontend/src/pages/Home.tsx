452e388b7b31662b7469aaa837a4dcd7acd10954
Dimitar Janevski <dimitar.j27@gmail.com>
2023-02-21 14:49:44 -0700

    add fonts + begin home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import hand from "../assets/hand.svg";
 
 function Home() {
-  return <div> home </div>;
+  return (
+    <div className="w-[100%] h-[100vh] flex">
+      <div className="w-[50%] h-[100vh] bg-blue text-white p-24 flex flex-col justify-center gap-5">
+        <div className="font-display text-8xl">Sketch Royale</div>
+        <div className="font-sans text-2xl">
+          Compete with your friends in this fun and challenging drawing game
+        </div>
+        <img src={hand} width="75%"></img>
+      </div>
+      <div className="w-[50%] h-[100vh] bg-red"></div>
+    </div>
+  );
 }
 
 export default Home;
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],theme: {
-    extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#FE5A43",
+        blue: "#03A5E6",
+        black: "#121212",
+      },
+      fontFamily: {
+        display: ["Bebas Neue", "cursive"],
+        sans: ["B612", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
